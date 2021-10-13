d27ca2cf0540ce69c53de5ec5bcaf73a0f2d7fd8
Ayush Bansal <bayush611@gmail.com>
2021-10-13 22:11:44 +0530

    Client Function Added

--- server.c
+++ server.c
@@ -42,3 +42,10 @@ void str_trim_lf (char* arr, int length) {
     }
   }
 }
+
+void print_client_addr(struct sockaddr_in addr){
+    printf("%d.%d.%d.%d",
+        addr.sin_addr.s_addr & 0xff,
+        (addr.sin_addr.s_addr & 0xff00) >> 8,
+        (addr.sin_addr.s_addr & 0xff0000) >> 16,
+        (addr.sin_addr.s_addr & 0xff000000) >> 24);
