2d16ba28af0319ee14d12a1b5930963421f86c33
Ayush Bansal <bayush611@gmail.com>
2021-10-13 22:08:47 +0530

    Client structure added

--- server.c
+++ server.c
@@ -9,3 +9,17 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <signal.h>
+
+#define MAX_CLIENTS 100
+#define BUFFER_SZ 2048
+
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
+
+/* Client structure */
+typedef struct{
+	struct sockaddr_in address;
+	int sockfd;
+	int uid;
+	char name[32];
+} client_t;
