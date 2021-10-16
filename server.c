83a1b944f42f53bc8c1750082cfb69c88bcf15b7
Sahil Bansal <bansalsahil740@gmail.com>
2021-10-16 17:53:22 +0530

    Socket setting

--- server.c
+++ server.c
@@ -156,3 +156,24 @@ void *handle_client(void *arg){
 
 	return NULL;
 }
+
+int main(int argc, char **argv){
+	if(argc != 2){
+		printf("Usage: %s <port>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	char *ip = "127.0.0.1";
+	int port = atoi(argv[1]);
+	int option = 1;
+	int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+  struct sockaddr_in cli_addr;
+  pthread_t tid;
+
+  /* Socket settings */
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(ip);
+  serv_addr.sin_port = htons(port);
+}
