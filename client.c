4df6bb7ec6a85a90962dee96f71d8bb5991205bc
Ayush Bansal <bayush611@gmail.com>
2021-10-17 16:10:50 +0530

    Handler Added

--- client.c
+++ client.c
@@ -30,3 +30,45 @@ void str_trim_lf (char* arr, int length) {
     }
   }
 }
+
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+void send_msg_handler() {
+  char message[LENGTH] = {};
+	char buffer[LENGTH + 32] = {};
+
+  while(1) {
+  	str_overwrite_stdout();
+    fgets(message, LENGTH, stdin);
+    str_trim_lf(message, LENGTH);
+
+    if (strcmp(message, "exit") == 0) {
+			break;
+    } else {
+      sprintf(buffer, "%s: %s\n", name, message);
+      send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+		bzero(message, LENGTH);
+    bzero(buffer, LENGTH + 32);
+  }
+  catch_ctrl_c_and_exit(2);
+}
+
+void recv_msg_handler() {
+	char message[LENGTH] = {};
+  while (1) {
+		int receive = recv(sockfd, message, LENGTH, 0);
+    if (receive > 0) {
+      printf("%s", message);
+      str_overwrite_stdout();
+    } else if (receive == 0) {
+			break;
+    } else {
+			// -1
+		}
+		memset(message, 0, sizeof(message));
+  }
+}
