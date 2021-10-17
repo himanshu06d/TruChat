1c0319d6a7061bce28af956cbed11b7481c9cc3e
himanshu06d <hd156681@gmail.com>
2021-10-17 12:01:04 +0530

    trim function added

--- client.c
+++ client.c
@@ -20,3 +20,13 @@ void str_overwrite_stdout() {
   printf("%s", "> ");
   fflush(stdout);
 }
+
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
