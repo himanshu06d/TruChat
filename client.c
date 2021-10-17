81a15a6e5c7321aee0fce7b868b7a990373a6e57
himanshu06d <hd156681@gmail.com>
2021-10-17 12:00:32 +0530

    global variable defined

--- client.c
+++ client.c
@@ -8,3 +8,15 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+
+#define LENGTH 2048
+
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char name[32];
+
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
+}
